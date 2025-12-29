8c1945e1bb4a45e62ae64a0f873f303d46ec321f
fatimasaci92-ui <fatimasaci92@gmail.com>
2025-12-29 14:33:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ import os
 import streamlit as st
 
 try:
-    # Charger la clé depuis les secrets
-    ee_key_json = json.loads(st.secrets["EE_KEY_JSON"])
-    
-    # Créer un fichier temporaire pour GEE
+    # Lire le JSON depuis les secrets
+    ee_key_json_str = st.secrets["EE_KEY_JSON"]["json"]  # note le ["json"]
+    ee_key_json = json.loads(ee_key_json_str)             # convertir en dict Python
+
+    # Créer un fichier temporaire
     with tempfile.NamedTemporaryFile(mode="w+", delete=False) as f:
         json.dump(ee_key_json, f)
         key_path = f.name
 
-    # Initialiser GEE avec la clé
+    # Initialiser GEE
     credentials = ee.ServiceAccountCredentials(
         ee_key_json["client_email"], key_path
     )
